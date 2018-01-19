711f156f2e14c41d7e0ffa6d6b750eb226c4ccc7
pao214 <bvsaikiranpatnaik@gmail.com>
2018-01-19 22:17:23 +0530

    Last commit.

--- src/twitter/SocialNetwork.java
+++ src/twitter/SocialNetwork.java
@@ -3,6 +3,13 @@
  */
 package twitter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -41,7 +48,29 @@ public class SocialNetwork {
      *         either authors or @-mentions in the list of tweets.
      */
     public static Map<String, Set<String>> guessFollowsGraph(List<Tweet> tweets) {
-        throw new RuntimeException("not implemented");
+        Map<String, Set<String>> resultGraph = new HashMap<>();
+        
+        for (Tweet tweet : tweets)
+        {
+            String author = tweet.getAuthor().toLowerCase();
+            Set<String> value = reduceSet(Extract.getMentionedUsers(Arrays.asList(tweet)));
+            
+            if (value.contains(author))
+            {
+                value.remove(author);
+            }
+            
+            if (resultGraph.containsKey(author))
+            {
+                resultGraph.get(author).addAll(value);
+            }
+            else
+            {
+                resultGraph.put(author, value);
+            }
+        }
+        
+        return resultGraph;
     }
 
     /**
@@ -54,7 +83,156 @@ public class SocialNetwork {
      *         descending order of follower count.
      */
     public static List<String> influencers(Map<String, Set<String>> followsGraph) {
-        throw new RuntimeException("not implemented");
-    }
+        Map<String, Set<String>> followedByGraph = invertGraph(followsGraph);
+        List<String> resultList = new ArrayList<>();
+        
+        for (Map.Entry<String, Set<String>> vertex : followedByGraph.entrySet())
+        {
+            if (!vertex.getValue().isEmpty())
+            {
+                resultList.add(vertex.getKey());
+            }
+        }
+        
+        Collections.sort(resultList, new Comparator<String>() {
 
+            @Override
+            public int compare(String o1, String o2) {
+                return followedByGraph.get(o2).size() - followedByGraph.get(o1).size();
+            }
+            
+        });
+        
+        return resultList;
+    }
+    
+    /**
+     * 
+     * @param neighbours set from which we extract the usernames, may not be modified
+     * @return the same set in lowercase
+     * @throws IllegalArgumentException when duplicate usernames present, username is case insensitive
+     */
+    public static Set<String> reduceSet(Set<String> neighbours)
+    {
+        Set<String> resultSet = new HashSet<String>();
+        
+        for (String neighbour : neighbours)
+        {
+            String value = neighbour.toLowerCase();
+            
+            if (resultSet.contains(value))
+            {
+                throw new IllegalArgumentException("Duplicate username in set");
+            }
+            
+            resultSet.add(value);
+        }
+        
+        return resultSet;
+    }
+    
+    /**
+     * 
+     * @param graph from which we extract the usernames and whom they follow, may not be modified
+     * @return all usernames in lowercase and remove empty sets
+     * @throws IllegalArgumentException when duplicate usernames are found, username is case insensitive
+     */
+    public static Map<String, Set<String>> reduceMap(Map<String, Set<String>> graph)
+    {
+        Map<String, Set<String>> resultGraph = new HashMap<String, Set<String>>();
+        
+        for (Map.Entry<String, Set<String>> vertex : graph.entrySet())
+        {
+            String key = vertex.getKey().toLowerCase();
+            Set<String> value = reduceSet(vertex.getValue());
+            
+            if (resultGraph.containsKey(key))
+            {
+                throw new IllegalArgumentException("Duplicate username in graph");
+            }
+            
+            if (!value.isEmpty())
+            {
+                resultGraph.put(key, value);
+            }
+        }
+        
+        return resultGraph;
+    }
+    
+    /**
+     * 
+     * @param followsGraph graph representing all the followers of a user
+     * @return the inverted graph in reduced form (all lowercase and empty users removed)
+     *          i.e. a vertex is present for a user iff he has atleast one follower
+     */
+    public static Map<String, Set<String>> invertGraph(Map<String, Set<String>> followsGraph)
+    {
+        Map<String, Set<String>> reducedGraph = reduceMap(followsGraph);
+        Map<String, Set<String>> followedByGraph = new HashMap<>();
+        
+        for (Map.Entry<String, Set<String>> vertex : reducedGraph.entrySet())
+        {
+            String follower = vertex.getKey();
+            Set<String> reducedSet = reduceSet(vertex.getValue());
+            
+            for (String user : reducedSet)
+            {
+                if (followedByGraph.containsKey(user))
+                {
+                    followedByGraph.get(user).add(follower);
+                }
+                else
+                {
+                    followedByGraph.put(user, new HashSet<String>(Arrays.asList(follower)));
+                }
+            }
+        }
+        
+        return followedByGraph;
+    }
+    
+    /**
+     * 
+     * @param influencers check whether this list is sorted in descending order, may not be modified
+     * @param followedByGraph the inverted graph representing the followers of a person, 
+     *         need to be in reduced form, may not be modified
+     * @return whether the given list is in fact sorted or not (does not need to be exhaustive)
+     */
+    public static boolean isSorted(List<String> influencers, Map<String, Set<String>> followedByGraph)
+    {        
+        if (!influencers.isEmpty())
+        {
+            Iterator<String> iter = influencers.iterator();
+            String author = iter.next().toLowerCase();
+            
+            if (!followedByGraph.containsKey(author))
+            {
+                throw new IllegalArgumentException("Author not present in the graph");
+            }
+            
+            int maxSize = followedByGraph.get(author).size();
+            
+            while (iter.hasNext())
+            {
+                author = iter.next().toLowerCase();
+                
+                if (!followedByGraph.containsKey(author))
+                {
+                    return false;
+                }
+                
+                int nextSize = followedByGraph.get(author).size();
+                
+                if (nextSize > maxSize)
+                {
+                    return false;
+                }
+                
+                maxSize = nextSize;
+            }
+        }
+        
+        return true;
+    }
 }
--- test/twitter/SocialNetworkTest.java
+++ test/twitter/SocialNetworkTest.java
@@ -5,8 +5,12 @@ package twitter;
 
 import static org.junit.Assert.*;
 
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -15,6 +19,16 @@ import org.junit.Test;
 
 public class SocialNetworkTest {
 
+    private static final Instant d1 = Instant.parse("2016-02-17T10:00:00Z");
+    
+    private static final Tweet tweet1 = new Tweet(1, "user1", "My inner self says, @user1 you are ready.", d1);
+    private static final Tweet tweet2 = new Tweet(2, "user2", "When sleeping I was in doubt, @user2 have you completed the job.", d1);
+    private static final Tweet tweet3 = new Tweet(3, "UseR3", "Now how do I explain the situation, @USER4?", d1);
+    private static final Tweet tweet4 = new Tweet(4, "uSEr4", "Boy! Aren't you worked up, @user3?", d1);
+    private static final Tweet tweet5 = new Tweet(5, "USER5", "Can you pass the message to @UseR3, @uSEr6?", d1);
+    private static final Tweet tweet6 = new Tweet(6, "user6", "Hi, @user3. Please don't get worked up. It is all good fun, you see", d1);
+    private static final Tweet tweet7 = new Tweet(7, "user6", "I can't find @user7. Can anyone tell me his location? @user4?", d1);
+    
     /*
      * TODO: your testing strategies for these methods should go here.
      * See the ic03-testing exercise for examples of what a testing strategy comment looks like.
@@ -26,19 +40,91 @@ public class SocialNetworkTest {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
     
+    /**
+     * 1. empty list of tweets
+     * 2. no dependancy
+     * 3. mentions himself
+     * 4. circular dependancy
+     * 5. non-circular dependancy
+     * 6. mentions users by various cases 
+     * 7. case change in tweet metadata
+     * 8. multiple tweets with same user
+     */
     @Test
     public void testGuessFollowsGraphEmpty() {
         Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(new ArrayList<>());
         
         assertTrue("expected empty graph", followsGraph.isEmpty());
+        
+        // Custom tests
+        Map<String, Set<String>> followsGraph1 = SocialNetwork.guessFollowsGraph(
+                Arrays.asList(tweet1, tweet2));
+        Map<String, Set<String>> resultGraph1 = Collections.emptyMap();
+        assertEquals("no dependancies, mentions himself", SocialNetwork.reduceMap(resultGraph1), 
+                SocialNetwork.reduceMap(followsGraph1));
+        
+        Map<String, Set<String>> followsGraph2 = SocialNetwork.guessFollowsGraph(
+                Arrays.asList(tweet3, tweet4, tweet5, tweet6));
+        Map<String, Set<String>> resultGraph2 = new HashMap<>();
+        resultGraph2.put("user3", new HashSet<>(Arrays.asList("user4")));
+        resultGraph2.put("user4", new HashSet<>(Arrays.asList("user3")));
+        resultGraph2.put("user5", new HashSet<>(Arrays.asList("user3", "user6")));
+        resultGraph2.put("user6", new HashSet<>(Arrays.asList("user3")));
+        assertEquals("circular dependancy", SocialNetwork.reduceMap(resultGraph2), 
+                SocialNetwork.reduceMap(followsGraph2));
+        
+        Map<String, Set<String>> followsGraph3 = SocialNetwork.guessFollowsGraph(
+                Arrays.asList(tweet3, tweet5, tweet6));
+        Map<String, Set<String>> resultGraph3 = new HashMap<>();
+        resultGraph3.put("user3", new HashSet<>(Arrays.asList("user4")));
+        resultGraph3.put("user5", new HashSet<>(Arrays.asList("user3", "user6")));
+        resultGraph3.put("user6", new HashSet<>(Arrays.asList("user3")));
+        assertEquals("no cirular dependancies", SocialNetwork.reduceMap(resultGraph3), 
+                SocialNetwork.reduceMap(followsGraph3));
+        
+        Map<String, Set<String>> followsGraph4 = SocialNetwork.guessFollowsGraph(
+                Arrays.asList(tweet6, tweet7));
+        Map<String, Set<String>> resultGraph4 = new HashMap<>();
+        resultGraph4.put("user6", new HashSet<>(Arrays.asList("user3", "user4", "user7")));
+        assertEquals("circular dependancy", SocialNetwork.reduceMap(resultGraph4), 
+                SocialNetwork.reduceMap(followsGraph4));
     }
     
+    /**
+     * 1. Empty graph
+     * 2. Graph containing just empty sets
+     * 3. Same number of followers
+     * 4. Distinct number of followers
+     */
     @Test
     public void testInfluencersEmpty() {
         Map<String, Set<String>> followsGraph = new HashMap<>();
         List<String> influencers = SocialNetwork.influencers(followsGraph);
         
         assertTrue("expected empty list", influencers.isEmpty());
+        
+        // Custom tests
+        Map<String, Set<String>> followsGraph1 = new HashMap<>();
+        followsGraph1.put("user1", Collections.emptySet());
+        List<String> influencers1 = SocialNetwork.influencers(followsGraph1);
+        assertTrue("just empty sets", SocialNetwork.isSorted(influencers1, 
+                SocialNetwork.invertGraph(followsGraph1)) && influencers1.size() == 0);
+        
+        Map<String, Set<String>> followsGraph2 = new HashMap<>();
+        followsGraph2.put("user1", new HashSet<String>(Arrays.asList("user2", "user3")));
+        followsGraph2.put("user2", new HashSet<String>(Arrays.asList("user3", "user1")));
+        followsGraph2.put("user3", new HashSet<String>(Arrays.asList("user1", "user2")));
+        List<String> influencers2 = SocialNetwork.influencers(followsGraph2);
+        assertTrue("just empty sets", SocialNetwork.isSorted(influencers2, 
+                SocialNetwork.invertGraph(followsGraph2)) && influencers2.size() == 3);
+        
+        Map<String, Set<String>> followsGraph3 = new HashMap<>();
+        followsGraph3.put("user1", new HashSet<String>(Arrays.asList("user2", "user3", "user4")));
+        followsGraph3.put("user2", new HashSet<String>(Arrays.asList("user3", "user4")));
+        followsGraph3.put("user3", new HashSet<String>(Arrays.asList("user4")));
+        List<String> influencers3 = SocialNetwork.influencers(followsGraph3);
+        assertTrue("just empty sets", SocialNetwork.isSorted(influencers3, 
+                SocialNetwork.invertGraph(followsGraph3)) && influencers3.size() == 3);
     }
 
     /*
